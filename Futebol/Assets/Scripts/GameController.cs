32a61fadde8d310ccda4aea1fc3e13e700cb19a0
Rafael Araujo <rafael0910i@gmail.com>
2022-01-21 18:11:58 -0300

    CAP3- game controller, field

--- Futebol/Assets/Scripts/GameController.cs
+++ Futebol/Assets/Scripts/GameController.cs
@@ -23,8 +23,14 @@ public class GameController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        Time.timeScale = 1;
         scoreBlue = 0;
         scoreRed = 0;
+        RedTurn = false;
+        panelGameOver.SetActive(false);
+        MenuManager = GameObject.Find("Menu Manager");
+        SetPlayer1Skins();
+        SetPlayer2Skins();
     }
 
     // Update is called once per frame
@@ -93,4 +99,91 @@ public class GameController : MonoBehaviour
             blueScoreText.text = scoreBlue.ToString();
         }
     }
+    
+    void SetPlayer1Skins()
+    {
+        switch (MenuManager.GetComponent<MenuManager>().p1ChoiceNum)
+        {
+            case 1:
+                blue1.GetComponent<SpriteRenderer>().sprite = option1;
+                blue2.GetComponent<SpriteRenderer>().sprite = option1;
+                blue3.GetComponent<SpriteRenderer>().sprite = option1;
+                blue4.GetComponent<SpriteRenderer>().sprite = option1;
+                blue5.GetComponent<SpriteRenderer>().sprite = option1;
+                blue6.GetComponent<SpriteRenderer>().sprite = option1;
+                blue7.GetComponent<SpriteRenderer>().sprite = option1;
+                break;
+            case 2:
+                blue1.GetComponent<SpriteRenderer>().sprite = option2;
+                blue2.GetComponent<SpriteRenderer>().sprite = option2;
+                blue3.GetComponent<SpriteRenderer>().sprite = option2;
+                blue4.GetComponent<SpriteRenderer>().sprite = option2;
+                blue5.GetComponent<SpriteRenderer>().sprite = option2;
+                blue6.GetComponent<SpriteRenderer>().sprite = option2;
+                blue7.GetComponent<SpriteRenderer>().sprite = option2;
+                break;
+            case 3:
+                blue1.GetComponent<SpriteRenderer>().sprite = option3;
+                blue2.GetComponent<SpriteRenderer>().sprite = option3;
+                blue3.GetComponent<SpriteRenderer>().sprite = option3;
+                blue4.GetComponent<SpriteRenderer>().sprite = option3;
+                blue5.GetComponent<SpriteRenderer>().sprite = option3;
+                blue6.GetComponent<SpriteRenderer>().sprite = option3;
+                blue7.GetComponent<SpriteRenderer>().sprite = option3;
+                break;
+            case 4:
+                blue1.GetComponent<SpriteRenderer>().sprite = option4;
+                blue2.GetComponent<SpriteRenderer>().sprite = option4;
+                blue3.GetComponent<SpriteRenderer>().sprite = option4;
+                blue4.GetComponent<SpriteRenderer>().sprite = option4;
+                blue5.GetComponent<SpriteRenderer>().sprite = option4;
+                blue6.GetComponent<SpriteRenderer>().sprite = option4;
+                blue7.GetComponent<SpriteRenderer>().sprite = option4;
+                break;
+        }
+    }
+    
+    void SetPlayer2Skins()
+    {
+        switch (MenuManager.GetComponent<MenuManager>().p2ChoiceNum)
+        {
+            case 1:
+                red1.GetComponent<SpriteRenderer>().sprite = option1;
+                red2.GetComponent<SpriteRenderer>().sprite = option1;
+                red2.GetComponent<SpriteRenderer>().sprite = option1;
+                red4.GetComponent<SpriteRenderer>().sprite = option1;
+                red5.GetComponent<SpriteRenderer>().sprite = option1;
+                red6.GetComponent<SpriteRenderer>().sprite = option1;
+                red7.GetComponent<SpriteRenderer>().sprite = option1;
+                break;
+            case 2:
+                red1.GetComponent<SpriteRenderer>().sprite = option2;
+                red2.GetComponent<SpriteRenderer>().sprite = option2;
+                red2.GetComponent<SpriteRenderer>().sprite = option2;
+                red4.GetComponent<SpriteRenderer>().sprite = option2;
+                red5.GetComponent<SpriteRenderer>().sprite = option2;
+                red6.GetComponent<SpriteRenderer>().sprite = option2;
+                red7.GetComponent<SpriteRenderer>().sprite = option2;
+                break;
+            case 3:
+                red1.GetComponent<SpriteRenderer>().sprite = option3;
+                red2.GetComponent<SpriteRenderer>().sprite = option3;
+                red2.GetComponent<SpriteRenderer>().sprite = option3;
+                red4.GetComponent<SpriteRenderer>().sprite = option3;
+                red5.GetComponent<SpriteRenderer>().sprite = option3;
+                red6.GetComponent<SpriteRenderer>().sprite = option3;
+                red7.GetComponent<SpriteRenderer>().sprite = option3;
+                break;
+            case 4:
+                red1.GetComponent<SpriteRenderer>().sprite = option4;
+                red2.GetComponent<SpriteRenderer>().sprite = option4;
+                red2.GetComponent<SpriteRenderer>().sprite = option4;
+                red4.GetComponent<SpriteRenderer>().sprite = option4;
+                red5.GetComponent<SpriteRenderer>().sprite = option4;
+                red6.GetComponent<SpriteRenderer>().sprite = option4;
+                red7.GetComponent<SpriteRenderer>().sprite = option4;
+                break;
+        }
+    }
+
 }
